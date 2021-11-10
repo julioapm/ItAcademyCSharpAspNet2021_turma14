ae9f967665a319d28838cf392b3ef3aeed281b3e
Júlio Pereira Machado <julioapm@gmail.com>
2021-11-10 15:14:48 -0300

    Código final

---
+++ Laboratorio1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+/*
+byte b;
+b = byte.MaxValue;
+Console.WriteLine("Valor máximo de byte:");
+Console.WriteLine(b);
+Console.WriteLine($"Valor máximo de byte: {b}");
+
+string strPrimeira = "Alô ";
+string strSegunda = "Mundo!";
+string strConcatenada = strPrimeira + strSegunda;
+Console.WriteLine(strConcatenada);
+Console.WriteLine(strConcatenada.Length);
+Console.WriteLine(strConcatenada.Substring(0, 5));
+Console.WriteLine(strPrimeira[0]);
+
+DateTime dt = new DateTime(2021, 11, 10);
+Console.WriteLine(dt);
+Console.WriteLine(dt.ToShortDateString());
+Console.WriteLine($"Hoje é {dt:dd-MM-yyyy}");
+*/
+
+int x = 0;
+int y = x;
+x++;
+Console.WriteLine(x);
+Console.WriteLine(y);
+
+
+StringBuilder s = new StringBuilder();
+Console.WriteLine(s);
+StringBuilder t = s;
+s.Append("Alô");
+Console.WriteLine(s);
+Console.WriteLine(t);
