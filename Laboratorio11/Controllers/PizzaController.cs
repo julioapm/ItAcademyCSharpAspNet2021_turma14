6a21c47a0a1fe94d3ee08a39cab26e5bc6933e8f
Júlio Pereira Machado <julioapm@gmail.com>
2021-11-23 17:16:46 -0300

    Código POST, PUT, DELETE

--- Laboratorio11/Controllers/PizzaController.cs
+++ Laboratorio11/Controllers/PizzaController.cs
@@ -34,4 +34,40 @@ public class PizzaController : ControllerBase
         }
         return pizza;
     }
+
+    [HttpPost]
+    public ActionResult<Pizza> Create(Pizza pizza)
+    {
+        _logger.LogInformation("Create pizza {pizza}", pizza);
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
+    }
+
+    [HttpPut("{id:int:min(1)}")]
+    public ActionResult Update(int id, Pizza pizza)
+    {
+        if (id != pizza.Id)
+        {
+            return BadRequest();
+        }
+        var pizzaToUpdate = PizzaService.Get(id);
+        if (pizzaToUpdate is null)
+        {
+            return NotFound();
+        }
+        PizzaService.Update(pizza);
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int:min(1)}")]
+    public ActionResult Delete(int id)
+    {
+        var pizza = PizzaService.Get(id);
+        if (pizza is null)
+        {
+            return NotFound();
+        }
+        PizzaService.Delete(id);
+        return NoContent();
+    }
 }
--- Laboratorio11/Models/Pizza.cs
+++ Laboratorio11/Models/Pizza.cs
@@ -1,8 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Laboratorio11.Models;
 
 public class Pizza
 {
     public int Id { get; set;}
+    [Required]
+    [StringLength(20)]
     public string? Name {get; set;}
     public bool IsGlutenFree {get; set;}
+    override public string ToString()
+    {
+        return $"{Id} - {Name} - {IsGlutenFree}";
+    }
 }
