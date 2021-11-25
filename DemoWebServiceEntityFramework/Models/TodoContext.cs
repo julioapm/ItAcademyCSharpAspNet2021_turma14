33d513c28adce09d6044f8b91e574e668ae302f4
Júlio Pereira Machado <julioapm@gmail.com>
2021-11-25 16:19:41 -0300

    Código do mapeamento

--- DemoWebServiceEntityFramework/Models/TodoContext.cs
+++ DemoWebServiceEntityFramework/Models/TodoContext.cs
@@ -14,4 +14,26 @@ public class TodoContext : DbContext
     }
 
     public DbSet<TodoItem> TodoItems { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TodoItem>()
+            .ToTable("Todos")
+            .HasKey(t => t.Id)
+            .HasName("PK_Todos");
+        modelBuilder.Entity<TodoItem>()
+            .Property(t => t.Name)
+            .HasMaxLength(200)
+            .IsRequired();
+        /*
+        modelBuilder.Entity<TodoItem>(eb => {
+            eb.ToTable("Todos")
+            .HasKey(t => t.Id)
+            .HasName("PK_Todos");
+            eb.Property(t => t.Name)
+            .HasMaxLength(200)
+            .IsRequired();
+        });
+        */
+    }
 }
