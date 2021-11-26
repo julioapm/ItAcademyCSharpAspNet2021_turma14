5f5bf1187e541f2f24eb154d19043eabc803b428
Júlio Pereira Machado <julioapm@gmail.com>
2021-11-26 14:24:49 -0300

    Códigos GET

--- DemoWebServiceEntityFramework/Controllers/TodoItemsController.cs
+++ DemoWebServiceEntityFramework/Controllers/TodoItemsController.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using DemoWebServiceEntityFramework.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DemoWebServiceEntityFramework.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class TodoItemsController : ControllerBase
 {
     private readonly ILogger<TodoItemsController> _logger;
@@ -15,4 +16,26 @@ public class TodoItemsController : ControllerBase
         _context = context;
     }
 
+    [HttpGet] //GET api/TodoItems
+    public async Task<IEnumerable<TodoItem>> GetTodoItems()
+    {
+        return await _context.TodoItems.ToListAsync();
+    }
+
+    [HttpGet("{id}")] //GET api/TodoItems/1
+    public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+    {
+        var todoItem = await _context.TodoItems.FindAsync(id);
+        if (todoItem == null)
+        {
+            return NotFound();
+        }
+        return todoItem;
+    }
+
+    [HttpGet] //GET api/TodoItems/notcomplete
+    public async Task<IEnumerable<TodoItem>> GetTodoItemsNotComplete()
+    {
+        return await _context.TodoItems.Where(t => !t.IsComplete).ToListAsync();
+    }
 }
