49fb48babf6e2b45cb55cd021c43fe1d3660c72e
Júlio Pereira Machado <julioapm@gmail.com>
2021-12-06 15:39:54 -0300

    Código engenharia reversa controlador de Customers

--- DemoWebServiceEntityFramework3/Program.cs
+++ DemoWebServiceEntityFramework3/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using DemoWebServiceEntityFramework3.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<NorthwindContext>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
